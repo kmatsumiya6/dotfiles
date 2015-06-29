443be364aa26586fe5477522fb776d04ebb8a9e4
kojiMac <kojiMac@wlan-026.asl.cs.ritsumei.ac.jp>
2015-06-29 18:41:44 +0900

    third commit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 DOT_FILES=(.vimrc .vim .tmux.conf .bashrc)
 
-for file in $(DOT_FILES[@])
+for file in ${DOT_FILES[@]}
 do
   rm -rf $HOME/$file
   ln -s $HOME/dotfiles/$file $HOME/$file
